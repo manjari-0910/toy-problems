3a1e1aa8fe0ae5c0a22d5271ba58a8735a903313
Manjari.Murala <muralamanjari@gmail.com>
2020-04-15 18:03:30 +0530

    Updated LRU_cache

--- LRU_cache.py
+++ LRU_cache.py
@@ -1,55 +1,25 @@
-class LRU:
-  def __init__(self, capacity):
-      self.capacity = capacity
-      self.elements = {}
-      self.elementKey = []
-      pass
-  
+class lru:
+    def __init__ (self,capacity):
+        super().__init__()
+        self.capacity = capacity
+        self.element = {}
+        self.elements = {}
 
-  def put(self, key, value):
-        if (len(self.elementKey) == self.capacity):
-              # x = elementKey.get(0)
-              x = self.elementKey.pop(0)
-              del self.elements[x]
-              self.elements[key] = value
-              self.elementKey.append(key)
+    def put(self,key,data):
+        if key not in self.element and len(self.element)==self.capacity:
+            old=min(self.elements.keys(),key=lambda k:self.elements[k])
+            self.element.pop(old)
+            self.elements.pop(old)
+        self.element[key] = data
+        self.elements[key]=1
+        return "Entered"
+
+    def get_cache(self):
+        return self.element
+
+    def get(self,key):
+        if key in self.element:
+            self.elements[key] += 1
+            return self.element[key]
         else:
-              self.elements[key] = value
-              self.elementKey.append(key)   
-  pass
-        
-
-  def get(self, key):
-        if(key in self.elements):
-              self.elementKey.remove(key)
-              self.elementKey.append(key)
-              val = self.elements[key]
-              del self.elements[key]
-              self.elements[key] = val
-              return self.elements[key]
-              
-        else:
-              return -1
-      
-  pass
-
-
-def main():
-      x1 = LRU(3)
-      x1.put('a', 2)
-      x1.put('b', 3)
-      x1.put('c', 4)
-      x1.put('d', 5)
-      print(list(x1.elements.keys()))
-      y = x1.get('b')
-      print(y)
-      print(list(x1.elements.keys()))
-      k = x1.get('c')
-      print(k)
-      print(list(x1.elements.keys()))
-
-
-pass
-
-if __name__ == '__main__':
-    main()
+            print("not found in cache")
